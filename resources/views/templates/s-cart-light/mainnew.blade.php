3fab2092277fb98470a3e927874a0893767b87c3
nitishibees <nitish@interactivebees.co>
2023-04-20 18:23:01 +0530

    hide slick slider liberary

--- resources/views/templates/s-cart-light/mainnew.blade.php
+++ resources/views/templates/s-cart-light/mainnew.blade.php
@@ -35,15 +35,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
-    <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/slick/slick.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css" integrity="sha512-nNlU0WK2QfKsuEmdcTwkeh+lhGs6uyOxuUs+n+0oXSYDok5qy0EI0lt01ZynHq6+p/tbgpZ7P+yUb+r71wqdXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/slick/slick.css')}}"> -->
      
     <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/main.css')}}">
     <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/responsive.css')}}">
-      
-    
-   
-
-
 
     <style>
         .ie-panel {
@@ -166,8 +162,9 @@
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/js/bootstrap-select.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js" integrity="sha512-uURl+ZXMBrF4AwGaWmEetzrd+J5/8NRkWAvJx5sbPSSuOb0bZLqf+tOzniObO00BjHa/dD7gub9oCGMLPQHtQA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     
-     <script src="{{ sc_file($sc_templateFile.'/css/slick/slick.min.js')}}"></script>
+     <!-- <script src="{{ sc_file($sc_templateFile.'/css/slick/slick.min.js')}}"></script> -->
      <script src="{{ sc_file($sc_templateFile.'/js/custom.js')}}"></script>
 
     <!-- js default for item s-cart -->
