5beb77d53f7d8a3911269095c2baa132584fa02c
nitishibees <nitish@interactivebees.co>
2023-04-21 17:12:12 +0530

    css preeload issue

--- resources/views/templates/s-cart-light/main.blade.php
+++ resources/views/templates/s-cart-light/main.blade.php
@@ -28,18 +28,15 @@
     <!--//Module header -->
     
     
-    <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/font-awesome.min.css')}}">
+    <link rel="preload" href="{{ sc_file($sc_templateFile.'/css/bootstrap.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="{{ sc_file($sc_templateFile.'/css/font-awesome.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
     
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css">
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css" integrity="sha512-nNlU0WK2QfKsuEmdcTwkeh+lhGs6uyOxuUs+n+0oXSYDok5qy0EI0lt01ZynHq6+p/tbgpZ7P+yUb+r71wqdXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    
-    <!-- <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/slick/slick.css')}}"> -->
-    <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/main.css')}}">
-    <link rel="stylesheet" href="{{ sc_file($sc_templateFile.'/css/responsive.css')}}">
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta3/dist/css/bootstrap-select.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.css" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="{{ sc_file($sc_templateFile.'/css/main.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+    <link rel="preload" href="{{ sc_file($sc_templateFile.'/css/responsive.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
 
     <style>
         .ie-panel {
